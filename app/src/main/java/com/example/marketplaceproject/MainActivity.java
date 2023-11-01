f8bd71f7e140eaf0334f021906f0d85247263ba5
ghazi23-dev <ghazismach24@gmail.com>
2023-11-01 22:00:14 +0100

    Project Layout Home Finished

--- app/src/main/java/com/example/marketplaceproject/MainActivity.java
+++ app/src/main/java/com/example/marketplaceproject/MainActivity.java
@@ -6,8 +6,10 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import com.example.marketplaceproject.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //PARAM FRAGMENTS
-
+//PARAM BIDING FRAGMENT EXCLUDED ADD BTN
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -29,14 +30,27 @@ public class MainActivity extends AppCompatActivity {
                 replaceFragment(new HomeFragment());
             } else if (item.getItemId() == R.id.list) {
                 replaceFragment(new ListFragment());
-            } else if (item.getItemId() ==R.id.Aboutus){
+            } else if (item.getItemId() == R.id.Aboutus) {
                 replaceFragment(new AboutFragment());
             }
             // Add more cases as needed for other menu items
             return true;
         });
+        // Find the FloatingActionButton // PERFORM NEW ACTION
+        FloatingActionButton addBtnForm = findViewById(R.id.addBtnForm);
 
+        // Set a click listener for the FloatingActionButton
+        addBtnForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the current fragment with the FormAddFragment
+                replaceFragment(new FormAddFragment());
+            }
+        });
     }
+
+
+
 
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -44,7 +58,5 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
-
-
 
 }
